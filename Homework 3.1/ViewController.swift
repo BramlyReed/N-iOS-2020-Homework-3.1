b2ef138a2ed09bc42a792d5bc248468aa690e36e
BramlyReed <ya.andreev99@ya.ru>
2020-11-22 23:52:31 -0800

    Initial Commit

---
+++ Homework 3.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 3.1
+//
+//  Created by Stas on 22.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
